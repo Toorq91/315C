c8475bb958b43f18394e039041a6cfdb5163fa5c
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-05-24 14:34:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FifteenC
+{
+    class ReverseText
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input text to be reversed: ");
+                var input = Console.ReadLine();
+                var reversed = ReverseString(input);
+                Console.WriteLine(reversed);
+            }
+        }
+
+        static string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
+
